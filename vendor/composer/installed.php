224973103c24951532598ec32221b58afdbaff89
nhpnd88 <nhpnd88@gmail.com>
2024-07-31 23:28:46 +0530

    third commit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '42c2a56d9df358d5750aacc48be9d2aa9706bf92',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '42c2a56d9df358d5750aacc48be9d2aa9706bf92',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
